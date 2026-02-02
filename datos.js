a39dca8c2a38ae7e20ea98aa8d35828b4edd418a
Jiarong <187628173+JiarongTangDaw@users.noreply.github.com>
2026-02-02 09:56:03 +0100

    Initial commit

---
+++ datos.js
@@ -0,0 +1,536 @@
+export const datos ={
+    "pagina0":{
+        "texto":"texto de la pagina 0",
+        "siguiente":"pagina1",
+        "anio": 2029,
+        "respuestas": []
+    },
+    "pagina1":{
+        "texto":"texto de la pagina 1",
+        "siguiente":"pagina2",
+        "anio": 2030,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1",
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina2":{
+        "texto":"texto de la pagina 2",
+        "siguiente":"pagina3",
+        "anio": 2031,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina3":{
+        "texto":"texto de la pagina 3",
+        "siguiente":"pagina4",
+        "anio": 2032,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2",
+                "puntuacion": -10,  
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina4":{
+        "texto":"texto de la pagina 4",
+        "siguiente":"pagina5",
+        "anio": 2033,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina5":{
+        "texto":"texto de la pagina 5",
+        "siguiente":"pagina6",
+        "anio": 2034,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina6":{
+        "texto":"texto de la pagina 6",
+        "siguiente":"pagina7",
+        "anio": 2035,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina7":{
+        "texto":"texto de la pagina 7",
+        "siguiente":"pagina8",
+        "anio": 2036,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina8":{
+        "texto":"texto de la pagina 8",
+        "siguiente":"pagina9",
+        "anio": 2037,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina9":{
+        "texto":"texto de la pagina 9",
+        "siguiente":"pagina10",
+        "anio": 2038,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina10":{
+        "texto":"texto de la pagina 10",
+        "siguiente":"pagina11",
+        "anio": 2039,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina11":{
+        "texto":"texto de la pagina 11",
+        "siguiente":"pagina12",
+        "anio": 2040,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina12":{
+        "texto":"texto de la pagina 12",
+        "siguiente":"pagina13",
+        "anio": 2041,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina13":{
+        "texto":"texto de la pagina 13",
+        "siguiente":"pagina14",
+        "anio": 2042,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina14":{
+        "texto":"texto de la pagina 14",
+        "siguiente":"pagina15",
+        "anio": 2043,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina15":{
+        "texto":"texto de la pagina 15",
+        "siguiente":"pagina16",
+        "anio": 2044,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina16":{
+        "texto":"texto de la pagina 16",
+        "siguiente":"pagina17",
+        "anio": 2045,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina17":{
+        "texto":"texto de la pagina 17",
+        "siguiente":"pagina18",
+        "anio": 2046,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina18":{
+        "texto":"texto de la pagina 18",
+        "siguiente":"pagina19",
+        "anio": 2047,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina19":{
+        "texto":"texto de la pagina 19",
+        "siguiente":"pagina20",
+        "anio": 2048,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina20":{
+        "texto":"texto de la pagina 20",
+        "siguiente":"pagina21",
+        "anio": 2049,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina21":{
+        "texto":"texto de la pagina 21",
+        "siguiente":"pagina22",
+        "anio": 2050,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina22":{
+        "texto":"texto de la pagina 22",
+        "siguiente":"pagina23",
+        "anio": 2051,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina23":{
+        "texto":"texto de la pagina 23",
+        "siguiente":"pagina24",
+        "anio": 2052,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina24":{
+        "texto":"texto de la pagina 24",
+        "siguiente":"pagina25",
+        "anio": 2053,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina25":{
+        "texto":"texto de la pagina 25",
+        "siguiente":"pagina26",
+        "anio": 2054,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina26":{
+        "texto":"texto de la pagina 26",
+        "siguiente":"pagina27",
+        "anio": 2055,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina27":{
+        "texto":"texto de la pagina 27",
+        "siguiente":"pagina28",
+        "anio": 2056,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina28":{
+        "texto":"texto de la pagina 28",
+        "siguiente":"pagina29",
+        "anio": 2057,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "pagina29":{
+        "texto":"texto de la pagina 29",
+        "siguiente":"pagina30",
+        "anio": 2058,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },    
+    "pagina30":{
+        "texto":"texto de la pagina 30",
+        "siguiente":"paginaFinal",
+        "anio": 2059,
+        "respuestas": [
+            {
+                "respuesta":"respuesta 1", 
+                "puntuacion": 10, 
+                "explicacion":"explicacion 1"
+            },
+            {
+                "respuesta":"respuesta 2", 
+                "puntuacion": -10, 
+                "explicacion":"explicacion 2"
+            }
+        ]
+    },
+    "paginaFinal":{
+        "texto":"texto de la pagina Final",
+        "siguiente":null,
+        "anio": 2060,
+        "respuestas": []
+    },
+    "finales": [
+        {"id": 1 , "texto" : "final 1"},
+        {"id": 2 , "texto" : "final 2"},
+        {"id": 3 , "texto" : "final 3"},
+        {"id": 4 , "texto" : "final 4"},
+        {"id": 5 , "texto" : "final 5"},
+        {"id": 6 , "texto" : "final 6"},
+        {"id": 7 , "texto" : "final 7"},
+        {"id": 8 , "texto" : "final 8"},
+        {"id": 9 , "texto" : "final 9"},
+        {"id": 10 , "texto" : "final 10"}
+    ]
+}
---
+++ funciones.js
@@ -0,0 +1,138 @@
+import { datos } from "./datos.js";
+
+let paginaActual = "";
+let puntuacionFinal = 0;
+
+function iniciarJuego(){
+    if(paginaActual === ""){
+        paginaActual = "pagina0";
+    }
+    mostrarPagina(paginaActual);
+}
+
+function mostrarPagina(paginaKey){
+    const pagina = datos[paginaKey];
+
+    let fin = comprobarFinal();
+    
+    if(!fin){
+        let contTexto = document.getElementById("contenedorTexto");
+        let h2 = document.createElement("h2");
+        h2.textContent = pagina.texto;
+        contTexto.appendChild(h2);
+
+        let contRespuestas = document.getElementById("contenedorRespuestas");
+        if(paginaKey === "pagina0"){
+            let botonSiguiente = document.createElement("button");
+            botonSiguiente.textContent = "Empezar";
+            botonSiguiente.onclick = function(){
+                paginaActual = pagina.siguiente;
+                limpiarPagina();
+                mostrarPagina(paginaActual);
+            };
+            contRespuestas.appendChild(botonSiguiente);
+        }else{
+            let posicionRespuesta = 0;
+            
+            for (const respuesta of pagina.respuestas) {
+                let posicion = posicionRespuesta;
+                let botonRespuesta = document.createElement("button");
+                botonRespuesta.textContent = respuesta.respuesta;
+                botonRespuesta.onclick = function(){
+                    puntuacionFinal += respuesta.puntuacion;
+                    console.log(puntuacionFinal);
+                    
+                    limpiarPagina();
+                    monstrarPaginaInfo(paginaActual,posicion);
+                };
+                contRespuestas.appendChild(botonRespuesta);
+                posicionRespuesta++;
+            }
+        }
+    }
+
+}
+
+function limpiarPagina(){
+    let contTexto = document.getElementById("contenedorTexto");
+    contTexto.innerHTML = "";
+    let contRespuestas = document.getElementById("contenedorRespuestas");
+    contRespuestas.innerHTML = "";
+}
+
+function monstrarPaginaInfo(paginaKey, posicionRespuesta){
+    const pagina = datos[paginaKey];
+    paginaActual = pagina.siguiente;
+    let contTexto = document.getElementById("contenedorTexto");
+    let h2 = document.createElement("h2");
+
+    h2.textContent = pagina.respuestas[posicionRespuesta].explicacion;
+    contTexto.appendChild(h2);
+
+    let contRespuestas = document.getElementById("contenedorRespuestas");   
+    let botonSiguiente = document.createElement("button");
+    botonSiguiente.textContent = "Continuar";
+    botonSiguiente.onclick = function(){
+        paginaActual = pagina.siguiente;
+        limpiarPagina();
+        mostrarPagina(paginaActual);
+    };
+    contRespuestas.appendChild(botonSiguiente);
+}
+
+function comprobarFinal(){
+    let pagina = datos[paginaActual];
+    let salida = false;
+    //logica para comprobar el final segun la puntuacionFinal
+    if(paginaActual != "paginaFinal"){
+        if(puntuacionFinal < 0){
+            limpiarPagina();
+            let contTexto = document.getElementById("contenedorTexto");
+            let anioFinal = pagina.anio;
+            let h2 = document.createElement("h2");
+            h2.textContent = "Has llegado al final. Te has quedado en el año " + anioFinal + " con la biodiversidad destruida.";
+            contTexto.appendChild(h2);
+
+            let contRespuestas = document.getElementById("contenedorRespuestas");   
+            let botonReiniciar = document.createElement("button");
+            botonReiniciar.textContent = "Reiniciar";
+            botonReiniciar.onclick = function(){
+                paginaActual = "pagina0";
+                puntuacionFinal = 0;
+                limpiarPagina();
+                iniciarJuego();
+            };
+            contRespuestas.appendChild(botonReiniciar);
+            salida = true;
+        }
+    }else{
+        let textoFinal = "";
+        limpiarPagina();
+        let contTexto = document.getElementById("contenedorTexto");
+        if(puntuacionFinal >= 0 && puntuacionFinal < 100){
+            textoFinal = datos["finales"][0].texto;
+        }else if(puntuacionFinal >= 100 && puntuacionFinal < 200){
+            textoFinal = datos["finales"][1].texto;
+        }else if(puntuacionFinal >= 200){
+            textoFinal = datos["finales"][2].texto;
+        }
+
+        let h2 = document.createElement("h2");
+        h2.textContent = textoFinal;
+        contTexto.appendChild(h2);
+
+        let contRespuestas = document.getElementById("contenedorRespuestas");   
+        let botonReiniciar = document.createElement("button");
+        botonReiniciar.textContent = "Volver a empezar";
+        botonReiniciar.onclick = function(){
+            paginaActual = "pagina0";
+            puntuacionFinal = 0;
+            limpiarPagina();
+            iniciarJuego();
+        };
+        contRespuestas.appendChild(botonReiniciar);
+        salida = true;
+    }
+    return salida;
+}
+iniciarJuego();
